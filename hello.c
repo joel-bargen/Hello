38bb7fd813eedddb65e690253146a9de2db85e59
Joel Bargen <jbargen3@unl.edu>
2020-08-13 18:55:25 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Joel Bargen
+ * Date: 2020
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello world!\n");
+     
+     return 0;
+     
+ }
